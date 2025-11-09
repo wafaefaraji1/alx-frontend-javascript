ce00e2759b631198134c6e2355d57b4b200e9753
wafae faraji <farajiwafae93@gmail.com>
2025-11-09 12:47:04 +0100

    Add all TypeScript tasks 0 to 5

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,47 +1,31 @@
+// ---------------- Interfaces ----------------
+
+// Teacher Interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any;
+  [key: string]: any; // يسمح لأي property إضافية
 }
 
+// Directors Interface extending Teacher
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-console.log(director1);
-
-// ---- New Part Below ----
-
-// Interface for the function
+// Interface for printTeacher function
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-// Function implementation
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
-
-console.log(printTeacher("John", "Doe")); // Output: J. Doe
-// -------------- Previous Code Above --------------
-
-// Interface for constructor
+// Interface for Student class constructor
 interface StudentClassConstructor {
   new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-// Interface describing the class
+// Interface describing Student class
 interface StudentClassInterface {
   firstName: string;
   lastName: string;
@@ -49,7 +33,27 @@ interface StudentClassInterface {
   displayName(): string;
 }
 
-// Class implementation
+// ---------------- Implementations ----------------
+
+// Director example
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+// printTeacher function implementation
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
+
+// StudentClass implementation
 class StudentClass implements StudentClassInterface {
   constructor(public firstName: string, public lastName: string) {}
 
@@ -62,7 +66,7 @@ class StudentClass implements StudentClassInterface {
   }
 }
 
-// Test
+// Test StudentClass
 const student = new StudentClass("Wafae", "Faraji");
 console.log(student.displayName());       // Wafae
 console.log(student.workOnHomework());    // Currently working
